8281d9726c4a81627cdda530dacb40ca8db3dd4c
Your Name <you@example.com>
2022-10-13 21:22:25 -0700

    changes in js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 main{
 
+    let problem one = 4
     function game{
 
 if problem one(4==4){
@@ -12,5 +13,20 @@ console.log('Out to the plank!')
 
     }
 
-let problem one = 4
 
+//describe('when answered is true', () => {
+//if answered, then it should show right and wrong buttons
+it('shows right and wrong buttons', () => {
+    const { getByText } = render(<ButtonHolder answeredStartsAs={true}/>);
+
+    const right = getByText(/right/i);
+    expect(right).toBeInTheDocument();
+
+    const wrong = getByText(/wrong/i);
+    expect(wrong).toBeInTheDocument();
+});
+
+//clicking right advances to next card
+
+//clicking wrong advances to next card
+});
