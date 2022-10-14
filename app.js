74db035826518f1fae1075040bf37ab1b2ae1e14
Your Name <you@example.com>
2022-10-13 20:45:30 -0700

    changes in documents

---
+++ app.js
@@ -0,0 +1,5 @@
+let horizon = window.innerHeight / 1.75
+let heightOfBackground = window.innerHeight-horizon
+
+
+tile('images/assets/sea-background-01.png', 0, horizon, window.innerWidth/100, heightOfBackground/100)
