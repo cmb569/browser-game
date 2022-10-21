0ef59f3d8ade1217c1729f77d911d7a5424827d8
Your Name <you@example.com>
2022-10-20 20:17:24 -0700

    changes to app.js

--- app.js
+++ app.js
@@ -148,34 +148,4 @@ incrementScore = num => {
 
 }
 
-/////////fullscreen button///////////
-let elem = document.getElementById("wholegame");
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-////////exit fullscreen button///////
-function closeFullscreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) { /* Safari */
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { /* IE11 */
-      document.msExitFullscreen();
-    }
-  }
-
-
-
-  /////////////////////////////////////
-
-
- 
-
 startGame()
