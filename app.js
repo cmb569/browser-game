1702355833bd748da5e500ef47ab1243ec74e469
Your Name <you@example.com>
2022-10-20 21:55:02 -0700

    re-add end page and css and js

--- app.js
+++ app.js
@@ -95,10 +95,10 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score)
 
-        //return window.location.assign('end.html')
+        return window.location.assign('/end.html')
     }
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
